a4e89f8f5502921195711c9db4dbe19069ab946c
Anii <79158719+lelouch-of-the-code@users.noreply.github.com>
2023-08-26 11:52:03 +0530

    first commit

---
+++ templates.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+#ifdef ANI
+#include "D:/DUSTBIN/local_inc.h"
+#else
+#define dbg(...) 0
+#endif
+
+void solve() {
+	
+}
+
+int main() {
+	int t=1;
+	cin>>t;
+	while(t--) {
+		solve();
+	}	
+}
